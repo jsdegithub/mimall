f0d4ca029d5820cba7a2117f571b5becd023231a
jsdegithub <def_myself@qq.com>
2020-07-26 16:08:42 +0800

    完成模态框的封装

--- mimall/src/pages/index.vue
+++ mimall/src/pages/index.vue
@@ -81,7 +81,7 @@
                                 <div class="item-info">
                                     <h3>{{item.name}}</h3>
                                     <p>{{item.subtitle}}</p>
-                                    <p class="price">{{item.price}}元</p>
+                                    <p class="price" @click="addCart(item.id)">{{item.price}}元</p>
                                 </div>
                             </div>
                         </div>
@@ -90,10 +90,16 @@
             </div>
         </div>
         <service-bar></service-bar>
+        <modal title="提示" sureText="查看购物车" btnType="1" modalType="middle" :showModal="showModal" @submit="goToCart" @cancel="showModal=false">
+            <template v-slot:body>
+                <p>商品添加成功</p>
+            </template>
+        </modal>
     </div>
 </template>
 <script>
 import ServiceBar from "../components/ServiceBar";
+import Modal from "../components/Modal";
 import { swiper, swiperSlide } from "vue-awesome-swiper";
 import "swiper/dist/css/swiper.css";
 export default {
@@ -101,6 +107,7 @@ export default {
         swiper,
         swiperSlide,
         ServiceBar,
+        Modal
     },
     data() {
         return {
@@ -193,6 +200,7 @@ export default {
                 },
             ],
             phoneList: [],
+            showModal:false
         };
     },
     mounted() {
@@ -204,16 +212,31 @@ export default {
                 .get("/products", {
                     params: {
                         categoryId: 100012,
-                        pageSize: 8,
+                        pageSize: 14,
                     },
                 })
                 .then((res) => {
+                    res.list = res.list.slice(6, 14);
                     this.phoneList = [
                         res.list.slice(0, 4),
                         res.list.slice(4, 8),
                     ];
                 });
         },
+        addCart(){
+            this.showModal=true;
+            /* this.axios.post('/carts',{
+                productId: id,
+                selected: true
+            }).then(()=>{
+
+            }).catch(()=>{
+                this.showModal=true;
+            }) */
+        },
+        goToCart(){
+            this.$router.push('/cart');
+        }
     },
 };
 </script>
