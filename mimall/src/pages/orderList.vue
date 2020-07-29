2c767c34ce371a6abcf6a0d8228394a064bd6446
jsdegithub <def_myself@qq.com>
2020-07-29 23:13:22 +0800

    订单页面加载动画和返回的订单列表为空的效果

--- mimall/src/pages/orderList.vue
+++ mimall/src/pages/orderList.vue
@@ -1,163 +1,187 @@
 <template>
-  <div class="order-list">
-    <order-header title="订单列表">
-      <template v-slot:tip>
-        <span>请谨防钓鱼链接或诈骗电话，了解更多></span>
-      </template>
-    </order-header>
-    <div class="wrapper">
-      <div class="container">
-        <div class="order-box">
-          <div class="order" v-for="(order, index) in list" :key="index">
-            <div class="order-title">
-              <div class="item-info fl">
-                {{order.createTime}}
-                <span>|</span>
-                {{order.receiverName}}
-                <span>|</span>
-                订单号：{{order.orderNo}}
-                <span>|</span>
-                {{order.paymentTypeDesc}}
-              </div>
-              <div class="item-money fr">
-                <span>应付金额：</span>
-                <span class="money">{{order.payment}}</span>
-                <span>元</span>
-              </div>
-            </div>
-            <div class="order-content clearfix">
-              <div class="good-box fl">
-                <div class="good-list" v-for="(item, i) in order.orderItemVoList" :key="i">
-                  <div class="good-img">
-                    <img v-lazy="item.productImage">
-                  </div>
-                  <div class="good-name">
-                    <div class="p-name">{{item.productName}}</div>
-                    <div class="p-money">{{item.totalPrice + 'x' + item.quantity}}元</div>
-                  </div>
+    <div class="order-list">
+        <order-header title="订单列表">
+            <template v-slot:tip>
+                <span>请谨防钓鱼链接或诈骗电话，了解更多></span>
+            </template>
+        </order-header>
+        <div class="wrapper">
+            <div class="container">
+                <div class="order-box">
+                    <loading v-if="loading"></loading>
+                    <div class="order" v-for="(order, index) in list" :key="index">
+                        <div class="order-title">
+                            <div class="item-info fl">
+                                {{order.createTime}}
+                                <span>|</span>
+                                {{order.receiverName}}
+                                <span>|</span>
+                                订单号：{{order.orderNo}}
+                                <span>|</span>
+                                {{order.paymentTypeDesc}}
+                            </div>
+                            <div class="item-money fr">
+                                <span>应付金额：</span>
+                                <span class="money">{{order.payment}}</span>
+                                <span>元</span>
+                            </div>
+                        </div>
+                        <div class="order-content clearfix">
+                            <div class="good-box fl">
+                                <div
+                                    class="good-list"
+                                    v-for="(item, i) in order.orderItemVoList"
+                                    :key="i"
+                                >
+                                    <div class="good-img">
+                                        <img v-lazy="item.productImage" />
+                                    </div>
+                                    <div class="good-name">
+                                        <div class="p-name">{{item.productName}}</div>
+                                        <div
+                                            class="p-money"
+                                        >{{item.totalPrice + 'x' + item.quantity}}元</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="good-state fr" v-if="order.status==20">
+                                <a href="javascript:;">{{order.statusDesc}}</a>
+                            </div>
+                            <div class="good-state fr" v-else>
+                                <a
+                                    href="javascript:;"
+                                    @click="goPay(order.orderNo)"
+                                >{{order.statusDesc}}</a>
+                            </div>
+                        </div>
+                    </div>
+                    <no-data v-if="!loading && list.length==0"></no-data>
                 </div>
-              </div>
-              <div class="good-state fr" v-if="order.status==20">
-                <a href="javascript:;">{{order.statusDesc}}</a>
-              </div>
-              <div class="good-state fr" v-else>
-                <a href="javascript:;" @click="goPay(order.orderNo)">{{order.statusDesc}}</a>
-              </div>
             </div>
-          </div>
         </div>
-      </div>
     </div>
-  </div>
 </template>
 <script>
-  import OrderHeader from './../components/OrderHeader'
-  export default{
-    name:'order-list',
-    components:{
-      OrderHeader
+import OrderHeader from "./../components/OrderHeader";
+import Loading from "./../components/Loading";
+import NoData from "./../components/NoData";
+export default {
+    name: "order-list",
+    components: {
+        OrderHeader,
+        Loading,
+        NoData
     },
     data() {
         return {
             list: [],
-        }
+            loading: true,
+        };
     },
-    mounted(){
+    mounted() {
         this.getOrderList();
     },
-    methods:{
-        getOrderList(){
-            this.axios.get('/orders').then(res => {
-                this.list=res.list;
-            })
+    methods: {
+        getOrderList() {
+            this.axios
+                .get("/orders")
+                .then((res) => {
+                    this.loading = false;
+                    this.list = res.list;
+                })
+                .catch((_) => {
+                    this.loading = false;
+                    this.$message.error("出错了，请稍后再试。");
+                });
         },
-        goPay(orderNo){
+        goPay(orderNo) {
             this.$router.push({
-                path: '/order/pay',
-                query:{
-                    orderNo
-                }
-            })
-        }
-    }
-  }
+                path: "/order/pay",
+                query: {
+                    orderNo,
+                },
+            });
+        },
+    },
+};
 </script>
 <style lang="scss">
-  @import './../assets/scss/config.scss';
-  @import './../assets/scss/mixin.scss';
-  .order-list{
-    .wrapper{
-      background-color:$colorJ;
-      padding-top:33px;
-      padding-bottom:110px;
-      .order-box{
-        .order{
-          @include border();
-          background-color:$colorG;
-          margin-bottom:31px;
-          &:last-child{
-            margin-bottom:0;
-          }
-          .order-title{
-            @include height(74px);
-            background-color:$colorK;
-            padding:0 43px;
-            font-size:16px;
-            color:$colorC;
-            .item-info{
-              span{
-                margin:0 9px;
-              }
-            }
-            .money{
-              font-size:26px;
-              color:$colorB;
-            }
-          }
-          .order-content{
-            padding:0 43px;
-            .good-box{
-              width:88%;
-              .good-list{
-                display: flex;
-                align-items: center;
-                height:145px;
-                .good-img{
-                  width:112px;
-                  img{
-                    width:100%;
-                  }
+@import "./../assets/scss/config.scss";
+@import "./../assets/scss/mixin.scss";
+.order-list {
+    .wrapper {
+        background-color: $colorJ;
+        padding-top: 33px;
+        padding-bottom: 110px;
+        .order-box {
+            .order {
+                @include border();
+                background-color: $colorG;
+                margin-bottom: 31px;
+                &:last-child {
+                    margin-bottom: 0;
                 }
-                .good-name{
-                  font-size:20px;
-                  color:$colorB;
+                .order-title {
+                    @include height(74px);
+                    background-color: $colorK;
+                    padding: 0 43px;
+                    font-size: 16px;
+                    color: $colorC;
+                    .item-info {
+                        span {
+                            margin: 0 9px;
+                        }
+                    }
+                    .money {
+                        font-size: 26px;
+                        color: $colorB;
+                    }
                 }
-              }
+                .order-content {
+                    padding: 0 43px;
+                    .good-box {
+                        width: 88%;
+                        .good-list {
+                            display: flex;
+                            align-items: center;
+                            height: 145px;
+                            .good-img {
+                                width: 112px;
+                                img {
+                                    width: 100%;
+                                }
+                            }
+                            .good-name {
+                                font-size: 20px;
+                                color: $colorB;
+                            }
+                        }
+                    }
+                    .good-state {
+                        @include height(145px);
+                        font-size: 20px;
+                        color: $colorA;
+                        a {
+                            color: $colorA;
+                        }
+                    }
+                }
             }
-            .good-state{
-              @include height(145px);
-              font-size: 20px;
-              color:$colorA;
-              a{
-                color:$colorA;
-              }
+            .pagination {
+                text-align: right;
             }
-          }
+            .el-pagination.is-background .el-pager li:not(.disabled).active {
+                background-color: #ff6600;
+            }
+            .el-button--primary {
+                background-color: #ff6600;
+                border-color: #ff6600;
+            }
+            .load-more,
+            .scroll-more {
+                text-align: center;
+            }
         }
-        .pagination{
-          text-align:right;
-        }
-        .el-pagination.is-background .el-pager li:not(.disabled).active{
-          background-color: #FF6600;
-        }
-        .el-button--primary{
-          background-color: #FF6600;
-          border-color: #FF6600;
-        }
-        .load-more,.scroll-more{
-          text-align:center;
-        }
-      }
     }
-  }
+}
 </style>
