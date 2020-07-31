c4e192ef0a0728fc17c4706406695ba886dd0b9b
jsdegithub <def_myself@qq.com>
2020-07-31 15:09:04 +0800

    移除import语句

--- mimall/src/router.js
+++ mimall/src/router.js
@@ -2,15 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './pages/home'
 import Index from './pages/index'
-import Product from './pages/product'
-import Detail from './pages/detail'
-import Login from './pages/login'
-import Cart from './pages/cart'
-import Order from './pages/order'
-import OrderConfirm from './pages/orderConfirm'
-import OrderList from './pages/orderList'
-import OrderPay from './pages/orderPay'
-import AliPay from './pages/alipay'
+
 Vue.use(Router);
 
 export default new Router({
